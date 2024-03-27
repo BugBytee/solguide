6f3c31e5f4088b690fd08a40b069c7c15d93ea45
bishakhne0gi <bneogi102002@gmail.com>
2024-03-27 09:33:38 +0530

    feat: fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,29 +1,42 @@
 import React from 'react'
-import Header from './Header'
+import Header from './header'
 import marquee from '../../public/data/marquee.json'
 import Image from 'next/image'
 import { Images } from '../../public/assets'
-import Marquee from "react-fast-marquee";
-
+import { usePathname } from 'next/navigation'
+import Marquee from 'react-fast-marquee'
 const Navbar = () => {
+
+
+    const pathname = usePathname();
+
+
+
     return (
         <>
             <Header />
-            <div className='navbar_container flex border-b-4 border-b-black flex-col sm:flex-row'>
-                <div className='marquee basis-[70%] flex items-center justify-center overflow-x-hidden border-b-4 sm:border-none'>
-                    <Marquee>
-                        {
-                            marquee.map((item, index) => (
-                                <>
+            <div className='navbar_container flex border-b-4 border-b-black'>
+                <div className='marquee basis-[70%] flex overflow-hidden items-center justify-center'>
 
-                                    <span key={index} className="mx-4 text-[40px] font-dmSans-medium">{item.items}</span>
-                                    <Image src={Images.marquee} className='mx-4' height={40} width={40} />
-                                </>
-                            ))
-                        }
-                    </Marquee>
+                    <div className="relative flex w-full overflow-x-hidden bg-white">
+
+                        <Marquee>
+                            {
+                                marquee.map((item, index) => (
+                                    <>
+
+                                        <span key={index} className="mx-4 text-[40px] font-dmSans-medium">{item.items}</span>
+                                        <Image src={Images.marquee} className='mx-4' height={40} width={40} />
+                                    </>
+                                ))
+                            }
+                        </Marquee>
+                    </div>
+
+
+
                 </div>
-                <div className='links flex items-center justify-center font-dmSans-medium bg-yellow-light text-[40px] sm:text-[56px] basis-[30%]  sm:border-l-4 '>
+                <div className='links flex items-center justify-center font-dmSans-bold bg-yellow-light text-[56px] basis-[30%] border-l-4 border-b-black'>
                     LOGIN
                 </div>
             </div>
